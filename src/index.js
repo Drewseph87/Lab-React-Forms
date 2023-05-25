76f6b02c4f1d4598bdaa5a402cdcd8c61ea62a2d
Vimaser <thfeske@gmail.com>
2023-05-25 18:55:51 -0500

    uncontrooled

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const Form = () => {
+  const handleSubmit = (event) => {
+    const username = event.target.username.value
+    console.log(username)
+    event.preventDefault();
+  }
+
   return (
     <div id='container'>
       <div id='navbar'>
         Form.js
       </div>
-      <form>
-        {/* your form fields here */}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='username'>Username:</label>
+        <input type='text' name='username' />
+        <button type='submit'>Submit:</button>
       </form>
     </div>
   )
