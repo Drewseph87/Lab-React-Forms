f6d2cce43c82baf8ea426611b90a65e4e431723b
Vimaser <thfeske@gmail.com>
2023-05-25 19:36:25 -0500

    multipleInputs

--- src/index.js
+++ src/index.js
@@ -2,18 +2,21 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 const Form = () => {
-  const [username, setUsername] = useState("");
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState(''); 
 
   const handleSubmit = (event) => {
     // const username = event.target.username.value;
     console.log(username);
     event.preventDefault();
     setUsername("");
+    setPassword("");
+    console.log(password);
   };
 
   const handleChange = (event) => {
     setUsername(event.target.value);
-    // console.log(setUsername);
+    //console.log(setIsername);
   };
 
   return (
@@ -26,6 +29,11 @@ const Form = () => {
           name="username"
           value={username}
           onChange={handleChange}
+        />
+        <label htmlFor='password'>Password:</label>
+        <input 
+        type='password' name='password' value={password} 
+        onChange={(e) => setPassword(e.target.value)} 
         />
         <button type="submit">Submit:</button>
       </form>
